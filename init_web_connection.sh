bc3c4a8c7d6f4af5702f35a226ce1cfcdbf29eec
Marcin Bodych <bodychmarcin@gmail.com>
2021-09-25 23:57:04 +0200

    small fixes

---
+++ init_web_connection.sh
@@ -0,0 +1,6 @@
+# For allowing other OS please visit https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+# this is for MAC OS:
+brew update && brew install azure-cli
+# this is for linux: curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+az login
--- publish_web_app.sh
+++ publish_web_app.sh
@@ -1,7 +1,1 @@
-# For allowing other OS please visit https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
-# this is for MAC OS:
-brew update && brew install azure-cli
-# this is for linux: curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
-az login
 az acr build --file Dockerfile --registry hybridfive2 --image hybridfiveimage .
