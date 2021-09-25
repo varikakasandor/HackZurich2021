3187e81acad0d1f4e2e1936ab5dc6b93954c2c3e
varikakasandor <varikakasandor@gmail.com>
2021-09-25 15:07:17 +0200

    Flask backend connected to frontend

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from waitress import serve
 
-app = Flask(__name__)
+from helper import *
+
+
+app = Flask(__name__,static_folder='static')
+app.config['UPLOAD_FOLDER'] = 'uploaded_contracts'
 
 @app.route("/")
-def hello_world():
-    return '<p>Hello, World!! <a href="team">Test link</a></p>'
+def home():
+    return app.send_static_file('application.html')
 
 @app.route("/team")
 def team_web():
-	return "this is our team"
+	return "<h1>This is our team! :) </h1>"
 
+@app.route('/contracts/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route('/process', methods=['POST'])
+def upload_contract():
+    contract = request.files['contract']
+    filename = secure_filename(contract.filename)
+    contract.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('download_file', name=filename)) #TODO: replace by calling the ML model and then returning the analyzed document
+
+
+
+if __name__=='__main__':
+    serve(app, host='127.0.0.1', port=8080)
